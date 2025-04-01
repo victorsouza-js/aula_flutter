30222460cc2f70781189cc57c6f29c9fb0864a6c
victorsouza-js <victorsouza5611@gmail.com>
2025-03-31 21:23:18 -0300

    add

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,62 +10,67 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    final TextEditingController _controller = TextEditingController();
+    final List<String> _tarefas = [];
+    final _formKey = GlobalKey<FormState>();
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blueAccent,
-        title: Text('AppBar', style: TextStyle(color: Colors.white,fontSize: 16,fontWeight: FontWeight.bold),),
+        title: Text(
+          'AppBar',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
         actions: [
-          IconButton(onPressed: (){}, icon: Icon(Icons.account_circle,color: Colors.white,))
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.add, color: Colors.white),
+          ),
         ],
       ),
 
       drawer: Drawer(),
       body: Column(
         children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Form(
-            child: TextField(
-            
-            decoration: InputDecoration(
-              prefixIcon: Icon(Icons.email),
-              border: OutlineInputBorder(
-                borderSide: BorderSide(
-                  color: Colors.black
-                ),
-                borderRadius: BorderRadius.circular(10)
-              ),
-          
-              hintText: 'E-mail',
-              hintStyle: TextStyle(color: Colors.black)
-          
-            
-            ),
-            
-          )),
-        ),
+          SizedBox(height: 40),
 
-        Padding(padding: const EdgeInsets.all(8.0),
-        child: Form(
-            child: TextField(
-            
-            decoration: InputDecoration(
-              prefixIcon: Icon(Icons.lock),
-              border: OutlineInputBorder(
-                borderSide: BorderSide(
-                  color: Colors.black
+          Center(child: Icon(Icons.account_circle, size: 60)),
+          SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Form(
+              child: TextFormField(
+                controller: _controller,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+
+                  labelText: 'Digitar item',
+                  labelStyle: TextStyle(color: Colors.black),
                 ),
-                borderRadius: BorderRadius.circular(10)
+
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Preencha corretamente';
+                  }
+                  return null;
+                },
               ),
-          
-              hintText: 'Senha',
-              hintStyle: TextStyle(color: Colors.black)
-          
-            
             ),
-            
-          )),
-        )
+          ),
+          SizedBox(height: 20),
+
+          ElevatedButton(
+            onPressed: () {
+              if (_formKey.currentState!.validate()) ;
+            },
+            child: Text('Adcionar'),
+          ),
         ],
       ),
     );
